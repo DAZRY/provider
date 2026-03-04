17732aa0e7275cbc505b68b31ae929825f447b91
Lendra <lendradzr77@gmail.com>
2026-03-04 10:29:44 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'injection.dart';
+import 'core/routes/app_router.dart';
 
 void main() {
-  runApp(
-    // Membungkus aplikasi dengan changenotifierprovider
-    //agar state bisa diakses dimana aja
-    ChangeNotifierProvider(
-      create: (context) => CartModel(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(buildApp());
 }
 
-//1. state model (business logic)
-class CartModel extends ChangeNotifier {
-  final List<String> _items = [];
-
-  List<String> get items => _items;
-
-  void add(String itemName) {
-    _items.add(itemName);
-    //code ini untuk beritahu UI untuk update
-    notifyListeners();
-  }
-
-  void removeAll() {
-    _items.clear();
-    notifyListeners();
-  }
-}
-
-// 2. UI Layer
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -38,121 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const MyCatalog(),
-        '/cart': (context) => const MyCart(),
-      },
-    );
-  }
-}
-
-// halaman katalog
-class MyCatalog extends StatelessWidget {
-  const MyCatalog({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final products = [
-      'Nasi Goreng',
-      'Sate Ayam',
-      'Es Teh',
-      'Ayam Bakar',
-      'Kopi',
-    ];
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Katalog Makanan'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () => Navigator.pushNamed(context, '/cart'),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(products[index]),
-            trailing: AddButton(item: products[index]),
-          );
-        },
-      ),
-    );
-  }
-}
-
-//widget tombol tambah
-class AddButton extends StatelessWidget {
-  final String item;
-  const AddButton({required this.item, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    //context.select mantau apa si item udh di keranjang
-    final isInCart = context.select<CartModel, bool>(
-      (cart) => cart.items.contains(item),
-    );
-
-    return TextButton(
-      onPressed: isInCart
-          ? null
-          : () {
-              //context.read digunakan untuk manggil fungsi tanpa
-              // mendengarkan perubahan
-              context.read<CartModel>().add(item);
-            },
-      child: isInCart
-          ? const Icon(Icons.check, color: Colors.green)
-          : const Text('TAMBAH'),
-    );
-  }
-}
-
-// Halaman Keranjang
-class MyCart extends StatelessWidget {
-  const MyCart({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // context.watch membuat widget
-    // ini re-build tiap kali CartModel berubah
-    var cart = context.watch<CartModel>();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Keranjang Belanja'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: cart.items.length,
-              itemBuilder: (context, index) =>
-                  ListTile(
-                    leading: const Icon(Icons.fastfood),
-                    title: Text(cart.items[index]),
-                  ),
-            ),
-          ),
-          const Divider(),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: () => cart.removeAll(),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: const Text(
-                'Hapus Keranjang',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ),
-        ],
-      ),
+      initialRoute: AppRouter.catalog,
+      routes: AppRouter.routes,
     );
   }
 }
