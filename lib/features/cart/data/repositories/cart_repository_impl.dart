dbf0dd2d843bfbb702ecac995228bd68de03260c
Lendra <lendradzr77@gmail.com>
2026-03-04 09:59:03 +0700

    cart_repository_impl

---
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -0,0 +1,17 @@
+// Implementasi konkret dari CartRepository
+// Di sini bisa diganti dengan API call, database, dll
+class CartRepositoryImpl implements CartRepository {
+  final List<Product> _items = [];
+
+  @override
+  List<Product> getCartItems() => List.unmodifiable(_items);
+
+  @override
+  void addItem(Product product) => _items.add(product);
+
+  @override
+
+  @override
+  bool isItemInCart(String productId) => 
+  _items.any((p) => p.id == productId);
+}
